de31ee0bdf5e7c6969fd887bf5728b7c696975de
GleidsonLM <gleidsonlm@gmail.com>
2023-06-29 19:33:38 -0300

    add: classification as object with min and max values

--- index.js
+++ index.js
@@ -1,76 +1,100 @@
 // Event listener for the form
-document.getElementById('formCalc').addEventListener('submit', calcularIMC);
+document.getElementById('formCalc').addEventListener('submit', CalculateBMI);
 // Event listener for the reset button
 document.getElementById('buttonReset').addEventListener('click', resetAll);
 
-// calcularIMC function receives the "peso" and "altura" values and calculates the BMI
-function calcularIMC(event) {
-    const peso = document.getElementById("peso").value;
-    const altura = document.getElementById("altura").value;
+// CalculateBMI function receives the "weight" and "height" values and calculates the BMI
+function CalculateBMI(event) {
+    const weight = document.getElementById("weight").value;
+    const height = document.getElementById("height").value;
 
-    if(peso === NaN || altura === NaN) {
+    if(weight === NaN || height === NaN) {
         alert("Por favor, preencha todos os campos");
         return resetAll();
     }
 
     event.preventDefault();
 
-    let imc;
-    if (document.getElementById("imc1").checked) {
-        imc = peso / Math.pow(altura, 2);
-    } else if (document.getElementById("imc2").checked) {
-        imc = (1.3 * peso) / Math.pow(altura, 2.5);
+    let bmi;
+    if (document.getElementById("bmi1").checked) {
+        bmi = weight / Math.pow(height, 2);
+    } else if (document.getElementById("bmi2").checked) {
+        bmi = (1.3 * weight) / Math.pow(height, 2.5);
     }
 
-    const imcFixed = imc.toFixed(2);
-    const imcElement = document.getElementById("imcElement");
-    imcElement.innerText = imcFixed;
+    const bmiFixed = bmi.toFixed(2);
+    const bmiElement = document.getElementById("bmiElement");
+    bmiElement.innerText = bmiFixed;
 
-    classificarIMC(imc);
+    ClassifyBmi(bmi);
 }
 
-// classificarIMC function receives the BMI value and classifies it
-function classificarIMC(imc) {
-    const imcClassElement = document.getElementById("imcClassElement");
-    const imcClassLibrary = {
-        subpeso : "você pode estar abaixo do peso ideal",
-        pesoIdeal : "você pode estar no peso ideal",
-        sobrepeso : "você pode estar em sobrepeso",
-        obesidade1 : "você pode estar em obesidade grau 1",
-        obesidade2 : "você pode estar em obesidade grau 2",
-        obesidade3 : "você pode estar em obesidade grau 3"
+// ClassifyBmi function receives the BMI value and classifies it
+function ClassifyBmi(bmi) {
+    const bmiClassificationElement = document.getElementById("bmiClassificationElement");
+    const bmiClassification = {
+        underweight : {
+            min: 0,
+            max: 18.5,
+            message: "você pode estar abaixo do peso ideal"
+        },
+        idealweight : {
+            min: 18.5,
+            max: 24.9,
+            message: "você pode estar no peso ideal"
+        },
+        overweight : {
+            min: 25,
+            max: 29.9,
+            message: "você pode estar em sobrepeso"
+        },
+        obesity1 : {
+            min: 30,
+            max: 34.9,
+            message: "você pode estar em obesidade grau 1"
+        },
+        obesity2 : {
+            min: 35,
+            max: 39.9,
+            message: "você pode estar em obesidade grau 2"
+        },
+        obesity3 : {
+            min: 40,
+            max: 40,
+            message: "você pode estar em obesidade grau 3"
+        }
     }
     
-    if (imc < 18.5) {
-        imcClassElement.innerText = imcClassLibrary.subpeso;
-    } else if (imc < 24.9) {
-        imcClassElement.innerText = imcClassLibrary.pesoIdeal;
-    } else if (imc < 29.9) {
-        imcClassElement.innerText = imcClassLibrary.sobrepeso;
-    } else if (imc < 34.9) {
-        imcClassElement.innerText = imcClassLibrary.obesidade1;
-    } else if (imc < 39.9) {
-        imcClassElement.innerText = imcClassLibrary.obesidade2;
+    if (bmi < bmiClassification.underweight.max) {
+        bmiClassificationElement.innerText = bmiClassification.underweight.message;
+    } else if (bmi < bmiClassification.idealweight.max) {
+        bmiClassificationElement.innerText = bmiClassification.idealweight.message;
+    } else if (bmi < bmiClassification.overweight.max) {
+        bmiClassificationElement.innerText = bmiClassification.overweight.message;
+    } else if (bmi < bmiClassification.obesity1.max) {
+        bmiClassificationElement.innerText = bmiClassification.obesity1.message;
+    } else if (bmi < bmiClassification.obesity2.max) {
+        bmiClassificationElement.innerText = bmiClassification.obesity2.message;
     } else {
-        imcClassElement.innerText = imcClassLibrary.obesidade3;
+        bmiClassificationElement.innerText = bmiClassification.obesity3.message;
     }
     
-    decorateClassificationBackground(imc);
+    decorateClassificationBackground(bmi);
 }
 
 // decorateClassificationBackground function receives the BMI value and decorates the background of the classification
-function decorateClassificationBackground(imc) {
-    const classificationBackground = document.getElementById("imcElement");
+function decorateClassificationBackground(bmi) {
+    const classificationBackground = document.getElementById("bmiElement");
     
-    if (imc < 18.5) {
+    if (bmi < 18.5) {
         classificationBackground.classList.add("notice");
-    } else if (imc < 24.9) {
+    } else if (bmi < 24.9) {
         classificationBackground.classList.add("log");
-    } else if (imc < 29.9) {
+    } else if (bmi < 29.9) {
         classificationBackground.classList.add("notice");
-    } else if (imc < 34.9) {
+    } else if (bmi < 34.9) {
         classificationBackground.classList.add("warning");
-    } else if (imc < 39.9) {
+    } else if (bmi < 39.9) {
         classificationBackground.classList.add("alert");
     } else {
         classificationBackground.classList.add("alert");
@@ -79,26 +103,26 @@ function decorateClassificationBackground(imc) {
     calcularPesoIdeal()
 }
 
-// calcularPesoIdeal function receives the "altura" value and calculates the ideal BMI
+// calcularPesoIdeal function receives the "height" value and calculates the ideal BMI
 function calcularPesoIdeal() {
-    const altura = document.getElementById("altura").value;
-    const elementoSuaAltura = document.getElementById("suaAltura");
-    elementoSuaAltura.innerText = altura;
+    const height = document.getElementById("height").value;
+    const heightElement = document.getElementById("heightElement");
+    heightElement.innerText = height;
 
-    let pesoIdealMin, pesoIdealMax;
-    if (document.getElementById("imc1").checked) {
-        pesoIdealMin = 18.5 * Math.pow(altura, 2);
-        pesoIdealMax = 25 * Math.pow(altura, 2);
-    } else if (document.getElementById("imc2").checked) {
-        pesoIdealMin = (18.5 / 1.3) * Math.pow(altura, 2.5);
-        pesoIdealMax = (25 / 1.3) * Math.pow(altura, 2.5);
+    let idealWeightMin, idealWeightMax;
+    if (document.getElementById("bmi1").checked) {
+        idealWeightMin = 18.5 * Math.pow(height, 2);
+        idealWeightMax = 25 * Math.pow(height, 2);
+    } else if (document.getElementById("bmi2").checked) {
+        idealWeightMin = (18.5 / 1.3) * Math.pow(height, 2.5);
+        idealWeightMax = (25 / 1.3) * Math.pow(height, 2.5);
     }        
 
-    const elementoPesoIdealMin = document.getElementById("pesoIdealMin");
-    elementoPesoIdealMin.innerText = Math.floor(pesoIdealMin);
+    const minIdealWeightElement = document.getElementById("idealWeightMin");
+    minIdealWeightElement.innerText = Math.floor(idealWeightMin);
     
-    const elementoPesoIdealMax = document.getElementById("pesoIdealMax");
-    elementoPesoIdealMax.innerText = Math.floor(pesoIdealMax);
+    const maxIdealWeightElement = document.getElementById("idealWeightMax");
+    maxIdealWeightElement.innerText = Math.floor(idealWeightMax);
 
     toggleResult()
 }
@@ -107,21 +131,22 @@ function calcularPesoIdeal() {
 function toggleResult() {
     const result = document.getElementById("result");
     result.classList.remove("hidden");
+    
     const calc = document.getElementById("calc");
     calc.classList.add("hidden");
 }
 
 // resetAll function resets all the values in the form and returns the "result" and "calc" <section> to its original state
 function resetAll() {
-    document.getElementById("peso").value = "";
-    document.getElementById("altura").value = "";
-    document.getElementById("imcElement").innerText = "";
-    document.getElementById("imcClassElement").innerText = "";
+    document.getElementById("weight").value = "";
+    document.getElementById("height").value = "";
+    document.getElementById("bmiElement").innerText = "";
+    document.getElementById("bmiClassificationElement").innerText = "";
     const result = document.getElementById("result");
     result.classList.add("hidden");
     const calc = document.getElementById("calc");
     calc.classList.remove("hidden");
-    const classificationBackground = document.getElementById("imcClassElement");
+    const classificationBackground = document.getElementById("bmiClassificationElement");
     classificationBackground.classList.remove("notice");
     classificationBackground.classList.remove("log");
     classificationBackground.classList.remove("warning");
